c85d77ef8085b86c400a5d96a918545715432b1a
Liang C <liang.cai@me.com>
2015-10-20 08:44:39 +0800

    v1937

--- lib/app.function.php
+++ lib/app.function.php
@@ -771,6 +771,7 @@ function phpmailer_send_mail(  $to , $subject , $body , $from ,  $host , $port ,
 
     if(!$mail->Send())
     {
+        $GLOBALS['LP_MAILER_ERROR'] = $mail->ErrorInfo;
         //echo $mail->ErrorInfo;
         return false;
     }
